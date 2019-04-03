66635450158081acfccbba39fd8e8a83f9e6c982
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-04-03 16:04:11 +0530

    h3.c

---
+++ h3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[200],i,j,n
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==i)
+        }
+    
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    return 0;
+}
+
