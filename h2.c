8a25068f3232027bbd1e42c30a39dc38d954cee9
Divyarrr <45899415+Divyarrr@users.noreply.github.com>
2019-04-03 15:51:58 +0530

    h2.c

---
+++ h2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+     int a[100],i,j,n,t;
+     scanf("%d",&n);
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&a[i]);
+         
+     }
+     for(i=0;i<n;i++)
+     {
+         for(j=i+1;j<n;j++)
+     {
+         if(a[i]>a[j])
+         {
+             t=a[i];
+             a[i]=a[j];
+             a[j]=t;
+         }
+         }
+     
+     }
+     for(i=n-1;i>=0;i--)
+     {
+         printf("%d ",a[i]);
+         
+     }
+     return 0;
+     
+     
+}
